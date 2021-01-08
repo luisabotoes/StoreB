38ff084f452d09306c15a69db6f9193a1fc2f107
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2021-01-08 17:27:44 +0000

    Init project

---
+++ StoreB.Common/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StoreB.Common
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ StoreB.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreB.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
