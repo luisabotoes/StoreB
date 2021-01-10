1a4df569464e901b98bc415720cd2fc9fe6c3674
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2021-01-10 17:28:46 +0000

    Create BD and Products controller

---
+++ StoreB.Web/Data/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace StoreB.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Product {
+        public int Id { get; set; }
+
+        [MaxLength(50,ErrorMessage ="The field {0} can only contain {1} characters length." )]
+        [Required]
+        public string Name { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+
+        [Display(Name = "Image")]
+        public decimal ImageUrl { get; set; }
+
+
+        [Display(Name = "Last Purchase")]
+        public DateTime? LastPurchase { get; set; }
+
+
+        [Display(Name = "Last Sale")]
+        public DateTime? LastSale { get; set; }
+
+
+        [Display(Name = "Is Available?")]
+        public bool IsAvailable { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public Double Stock { get; set; }
+    }
+}
--- StoreB.Web/Startup.cs
+++ StoreB.Web/Startup.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using StoreB.Web.Data;
 
 namespace StoreB.Web
 {
@@ -24,6 +21,13 @@ namespace StoreB.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+
+            });
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
